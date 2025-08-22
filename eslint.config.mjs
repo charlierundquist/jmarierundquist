48b7278be01239fcada54b68c71422e90a006b35
charlierundquist <143918996+charlierundquist@users.noreply.github.com>
2025-08-22 11:52:57 -0500

    turning off eslint for compile erorrs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,23 +12,23 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
-    rules: {
-      '@typescript-eslint/ban-ts-comment': 'warn',
-      '@typescript-eslint/no-empty-object-type': 'warn',
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          vars: 'all',
-          args: 'after-used',
-          ignoreRestSiblings: false,
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          destructuredArrayIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^(_|ignore)',
-        },
-      ],
-    },
+    // rules: {
+    //   '@typescript-eslint/ban-ts-comment': 'warn',
+    //   '@typescript-eslint/no-empty-object-type': 'warn',
+    //   '@typescript-eslint/no-explicit-any': 'warn',
+    //   '@typescript-eslint/no-unused-vars': [
+    //     'warn',
+    //     {
+    //       vars: 'all',
+    //       args: 'after-used',
+    //       ignoreRestSiblings: false,
+    //       argsIgnorePattern: '^_',
+    //       varsIgnorePattern: '^_',
+    //       destructuredArrayIgnorePattern: '^_',
+    //       caughtErrorsIgnorePattern: '^(_|ignore)',
+    //     },
+    //   ],
+    // },
   },
   {
     ignores: ['.next/'],
