7b44a0757d5a746cc0c1a98dd35aadc94e9da3d2
charlierundquist <143918996+charlierundquist@users.noreply.github.com>
2025-08-27 12:58:36 -0500

    added mailerlite and better build command

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -4,6 +4,7 @@ import { Header } from '../components/Header/Component'
 import { Footer } from '../components/Footer/Component'
 import { Noto_Sans, Noto_Serif } from 'next/font/google'
 import { CTABlock } from '../components/CTABlock/Component'
+import Script from 'next/script'
 
 const notoSans = Noto_Sans({
   subsets: ['latin'],
@@ -26,6 +27,9 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
 
   return (
     <html lang="en" className={`${notoSans.variable} ${notoSerif.variable}`}>
+      <head>
+        <Script src="@utilities/mailerLite.js"></Script>
+      </head>
       <body suppressHydrationWarning>
         <Header></Header>
         <main>{children}</main>
--- src/app/components/CTABlock/Component.tsx
+++ src/app/components/CTABlock/Component.tsx
@@ -21,9 +21,18 @@ export async function CTABlock() {
         {ctaBlock.ctaSubtitle && (
           <RichText data={ctaBlock.ctaSubtitle} className="text-center text-balance" />
         )}
-        {ctaBlock.ctaLinks && (
+        {/* {ctaBlock.ctaLinks && (
           <DisplayLinks links={ctaBlock.ctaLinks} className="justify-center"></DisplayLinks>
-        )}
+        )} */}
+        <button
+          className={`ml-onclick-form cmslink focus-visible:-translate-y-0.5' bg-pink w-fit rounded-full px-4 py-1 transition-transform duration-200 hover:-translate-y-0.5 focus:-translate-y-0.5`}
+          onClick={() => {
+            // @ts-expect-error mailerlite
+            ml('show', 'BDr1cA', true)
+          }}
+        >
+          Subscribe
+        </button>
       </>
     ),
   }
